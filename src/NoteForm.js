a3a0b0867e51d85575b35362d4a883a74c4ba2da
Dave Strus <dave@getfretless.com>
2017-05-11 16:35:34 -0400

    Appearance tweaks

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -32,25 +32,35 @@ class NoteForm extends Component {
     const note = {...this.state.note}
     note[ev.target.name] = ev.target.value
     this.setState({ note }, () => this.props.saveNote(this.state.note))
-    
   }
 
   render() {
     const note = this.state.note
     return (
       <div className="NoteForm">
+        <div className="form-actions">
+          <button type="button" onClick={() => this.props.removeNote(this.state.note)}>
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
         <form>
           <p>
-            <input type="text" name="title" onChange={this.handleChanges} value={this.state.note.title} />
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note"
+              onChange={this.handleChanges}
+              value={this.state.note.title}
+            />
           </p>
           <p>
-            <textarea name="" id="" cols="30" rows="10" name="body" onChange={this.handleChanges} value={note.body}></textarea>
+            <textarea
+              name="body"
+              placeholder="Just start typing..."
+              onChange={this.handleChanges}
+              value={note.body}
+            ></textarea>
           </p>
-          <div className="form-actions">
-            <button type="button" onClick={() => this.props.removeNote(this.state.note)}>
-              <i className="fa fa-trash"></i>
-            </button>
-          </div>
         </form>
       </div>
     )
